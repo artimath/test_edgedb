1222584bbf25a16001534609f2c0762d9d19d3b1
artimath <ryanjhunter@gmail.com>
2023-05-28 17:19:18 -1000

    Initial commit

---
+++ lib/test_edgedb.ex
@@ -0,0 +1,18 @@
+defmodule TestEdgedb do
+  @moduledoc """
+  Documentation for `TestEdgedb`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestEdgedb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/test_edgedb_test.exs
@@ -0,0 +1,8 @@
+defmodule TestEdgedbTest do
+  use ExUnit.Case
+  doctest TestEdgedb
+
+  test "greets the world" do
+    assert TestEdgedb.hello() == :world
+  end
+end
